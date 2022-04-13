ff01833a8f584387c462c52f072e1274bfc21324
Alexey Cherkashin <goodniceweb@gmail.com>
2022-04-13 17:04:54 +0300

    Fix

--- git.go
+++ git.go
@@ -9,9 +9,10 @@ import (
 func getRawBranches(gitExec string, mainGitArgs []string) []string {
 	stdout := runCommandWithHandledOutput(gitExec, mainGitArgs)
 	branches := strings.Split(string(stdout), "\n")
+  branches = cutLast(branches)
 	min := math.Min(float64(len(branches)), float64(globalConfig.AmountOfBranches))
 	branches = branches[0:int(min)]
-	return cutLast(branches)
+	return branches
 }
 
 func switchToBranch(gitExec string, gitArgs []string) {
